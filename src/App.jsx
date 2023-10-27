ecdb12645e2e4db336d070a38a0327e09aa5c874
Otto Adelborg <otto.adelborg@gmail.com>
2023-10-27 15:23:10 +0200

    Added error test

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,83 @@
-import './App.css'
-import {useState} from 'react';
-
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  // State för när användaren skriver i input och söker efter det i API
+  const [search, setSearch] = useState("");
+  // State för try/if som ligger med API fetch
+  const [error, setError] = useState("");
+  // State som sparar info om vad som kommer tillbaka från API fetchen
+  const [results, setResults] = useState([]);
 
-// State för när användaren skriver i input och söker efter det i API
-const [ search, setSearch ] = useState("")
-// State för try/if som ligger med API fetch
-const [ error, setError ] = useState("")
-// State som sparar info om vad som kommer tillbaka från API fetchen
-const [ results, setResults ] = useState([]);
+  // API fetch med try/if om användaren skriver ett ord som inte finns eller finns-
+  // plockar if upp
+  async function WordFinder() {
+    try {
+      const response = await fetch(
+        `https://api.dictionaryapi.dev/api/v2/entries/en/${search}`
+      );
+      if (!response.ok) {
+        throw new error("Word cannot be found");
+      }
+      const data = await response.json();
+      setResults(data);
+      console.log(data);
+    } catch (error) {
+      setError("Word cannot be found");
+    }
+  }
 
-
-// API fetch med try/if om användaren skriver ett ord som inte finns eller finns-
-// plockar if upp
-async function WordFinder() {
-  
-  try {
-  const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${search}`);
-  if(!response.ok) {
-    throw new error('Word cannot be found')
-}
-  const data = await response.json();
-  setResults(data);
-  console.log(data);
- } catch (error) {
-  setError('Word cannot be found')
-}
-}
-
-
-// Passerar audioURL direkt för att kalla den genom knappen
-function PlayAudio(audioUrl) {
-  let audio = new Audio(audioUrl);
-  audio.play();
-}
-
+  // Passerar audioURL direkt för att kalla den genom knappen
+  function PlayAudio(audioUrl) {
+    let audio = new Audio(audioUrl);
+    audio.play();
+  }
 
   return (
-    <div className='App'> 
-    <h1>WORD GENERATOR</h1>
-   <section className='input--Wrapper'>
-      <input onChange={ (event) => {setSearch(event.target.value)} } data-testid="searchbar"/>
-      <button onClick={ WordFinder } className='search--button' data-testid="searchbutton">Search</button>
-      {error? <p className='error--message'>{error}</p>: null}
-            <section className='result--container'>
-{/* Mappar ut allt som jag behöver från setResults state */}
-            {results.map((result, index) => (
-              <div key={index}>
-                <h2 data-testid="word">{result?.word}</h2>
-                <p>{error?.title}</p>
-                <p>{ result.meanings?.antonyms }</p>
-                <p data-testid="definition">Definition: { result.meanings[0].definitions[0].definition }</p>
-                <p>Synonyms: { result.meanings[0].definitions[0].synonyms[0] }</p>
-                <p>Example: { result.meanings[0].definitions[0].example }</p>
-                <p>Source:  { result?.sourceUrls}</p>
-                <button onClick={() => PlayAudio(result.phonetics[0].audio)} className='audio--button' data-testid="audiobutton">Ljud</button>
-                <hr />
-              </div>
-            ))}
-            </section>
-    </section>
+    <div className="App">
+      <h1>WORD GENERATOR</h1>
+      <section className="input--Wrapper">
+        <input
+          onChange={(event) => {
+            setSearch(event.target.value);
+          }}
+          data-testid="searchbar"
+        />
+        <button
+          onClick={WordFinder}
+          className="search--button"
+          data-testid="searchbutton"
+        >
+          Search
+        </button>
+        {error ? <p className="error--message">{error}</p> : null}
+        <section className="result--container">
+          {/* Mappar ut allt som jag behöver från setResults state */}
+          {results.map((result, index) => (
+            <div key={index}>
+              <h2 data-testid="word">{result?.word}</h2>
+              <p>{error?.title}</p>
+              <p>{result.meanings?.antonyms}</p>
+              <p data-testid="definition">
+                Definition: {result.meanings[0].definitions[0].definition}
+              </p>
+              <p>Synonyms: {result.meanings[0].definitions[0].synonyms[0]}</p>
+              <p>Example: {result.meanings[0].definitions[0].example}</p>
+              <p>Source: {result?.sourceUrls}</p>
+              <button
+                onClick={() => PlayAudio(result.phonetics[0].audio)}
+                className="audio--button"
+                data-testid="audiobutton"
+              >
+                Ljud
+              </button>
+              <hr />
+            </div>
+          ))}
+        </section>
+      </section>
     </div>
-  )
+  );
 }
 
-
-export default App
+export default App;
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,53 +1,62 @@
-import '@testing-library/jest-dom'
-import { render, fireEvent, screen } from '@testing-library/react'
+import "@testing-library/jest-dom";
+import { render, fireEvent, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import App from './App';
+import App from "./App";
 
+test("renders main app", () => {
+  render(<App />);
+  expect(true).toBeTruthy();
+});
 
-test('renders main app', () => {
-    render(<App/>);
-    expect(true).toBeTruthy()
-})
+describe("Checking mainpage header text", () => {
+  it("the title should be visible", () => {
+    render(<App />);
+    expect(screen.getByText(/WORD GENERATOR/i)).toBeDefined();
+  });
+});
 
-describe('Checking mainpage header text', () => {
-    it('the title should be visible', () => {
-        render(<App/>)
-        expect(screen.getByText(/WORD GENERATOR/i)).toBeDefined()
-    })
-})
-
-describe('Checking input element', () => {
-    it('renders input element', () => {
-      const { getByTestId } = render(<App />);
-      const input = getByTestId('searchbar');
-      expect(input).toBeInTheDocument();
-    });
-  
-    it('updates input value', () => {
-      const { getByTestId } = render(<App />);
-      const input = getByTestId('searchbar');
-      fireEvent.change(input, { target: { value: 'House' } });
-      expect(input.value).toBe('House');
-    });
+describe("Checking input element", () => {
+  it("renders input element", () => {
+    const { getByTestId } = render(<App />);
+    const input = getByTestId("searchbar");
+    expect(input).toBeInTheDocument();
   });
 
-describe('Searching for word', () => {
-    it('Types in input and clicks button', async () => {
-        const { getByTestId } = render(<App/>);
-        const input = getByTestId('searchbar');
-        fireEvent.change(input, { target: { value: 'Tree' } });
-        userEvent.click(screen.getByTestId("searchbutton"));
-        await screen.findByText('tree');
-    })
-})
+  it("updates input value", () => {
+    const { getByTestId } = render(<App />);
+    const input = getByTestId("searchbar");
+    fireEvent.change(input, { target: { value: "House" } });
+    expect(input.value).toBe("House");
+  });
+});
 
-describe('Searching for word and gets a desc', () => {
-    it('types in a word and get out a desc for the word used', async () => {
-        const { getByTestId } = render(<App/>);
-        const input = getByTestId('searchbar');
-        fireEvent.change(input, { target: {value: 'food'} });
-        userEvent.click(screen.getByTestId("searchbutton"));
-        await screen.findByText('food')
-        await screen.getByTestId('definition')
-    })
-})
+describe("Searching for word", () => {
+  it("Types in input and clicks button", async () => {
+    const { getByTestId } = render(<App />);
+    const input = getByTestId("searchbar");
+    fireEvent.change(input, { target: { value: "Tree" } });
+    userEvent.click(screen.getByTestId("searchbutton"));
+    await screen.findByText("tree");
+  });
+});
+
+describe("Searching for word and gets a desc", () => {
+  it("types in a word and get out a desc for the word used", async () => {
+    const { getByTestId } = render(<App />);
+    const input = getByTestId("searchbar");
+    fireEvent.change(input, { target: { value: "food" } });
+    userEvent.click(screen.getByTestId("searchbutton"));
+    await screen.findByText("food");
+    await screen.getByTestId("definition");
+  });
+});
+
+describe("Searching for wrong word", () => {
+  it("Should give user a error message", async () => {
+    const { getByTestId } = render(<App />);
+    const input = getByTestId("searchbar");
+    fireEvent.change(input, { target: { value: "asdasd" } });
+    userEvent.click(screen.getByTestId("searchbutton"));
+    await screen.findByText("Word cannot be found");
+  });
+});
